f756156a556f766df6c0e0e59cc83af2a58f16ad
Vontrailop <62673861+Vontrailop@users.noreply.github.com>
2022-03-16 14:52:53 -0600

    ya te vi Rodo

--- src/components/account/UserInfo.js
+++ src/components/account/UserInfo.js
@@ -1,96 +1,117 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
-import { Avatar } from 'react-native-elements'
-import * as firebase from 'firebase'
-import * as Permissions from 'expo-permissions'
-import * as ImagePicker from 'expo-image-picker'
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { Avatar } from "react-native-elements";
+import * as firebase from "firebase";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
 
 export default function UserInfo(props) {
-    const {userInfo: { uid, photoURL, displayName, email}, 
-            toastRef} = props;
-    
-    const changeAvatar = async ( ) =>{
-        const resultPermission = await Permissions.askAsync(Permissions.CAMERA);
-        const resultPermissionCamera = resultPermission.permissions.camera.status;
-        if (resultPermissionCamera == 'denied') {
-            toastRef.current.show("Se necesitan permisos de camará")
-        }else{
-            const result = await ImagePicker.launchImageLibraryAsync({
-                allowsEditing: true,
-                aspect: [4,3]
-            })
-            if (result.cancelled) {
-                toastRef.current.show("No has seleccionado imagén")
-            }else{
-                uploadImage(result.uri)
-                .then(() => {
-                    toastRef.current.show("Imagén subida")
-                    updatePhotoUrl();
-                })
-            }
-        }
-    }
+  const {
+    userInfo: { uid, photoURL, displayName, email },
+    toastRef,
+    setLoading,
+    setLoadingText,
+  } = props;
 
-    const uploadImage = async (uri) => {
-        //console.log(uri)
-        const response = await fetch(uri);
-        //Recuperamos el blob de la imagen
-        const blob = await response.blob();
-        //Guardamos en el storage
-        const ref = firebase.storage().ref().child(`avatar/${uid}`)
-        //Subimos el cambio
-        return ref.put(blob);
+  const changeAvatar = async () => {
+    const resultPermission = await Permissions.askAsync(Permissions.CAMERA);
+    const resultPermissionCamera = resultPermission.permissions.camera.status;
+    if (resultPermissionCamera == "denied") {
+      toastRef.current.show("Se necesitan permisos de camará");
+    } else {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+      });
+      if (result.cancelled) {
+        toastRef.current.show("No has seleccionado imagén");
+      } else {
+        uploadImage(result.uri)
+          .then(() => {
+            updatePhotoUrl();
+            toastRef.current.show("Imagén subida");
+          })
+          .catch((err) => {
+            console.log(err);
+            toastRef.current.show("Error al cargar la imagen");
+          });
+      }
     }
+  };
 
-    const updatePhotoUrl = () =>{
-        firebase.storage().ref(`avatar/${uid}`).getDowbLoadURL()
-        .then( async (response) => {
-            console.log(response)
-        })
-        .catch(() => {
-            toastRef.current.show("Error al actualizar imagen")
-        })
-    }
+  const uploadImage = async (uri) => {
+    setLoading(true);
+    setLoadingText("Cargando Avatar");
+    //console.log(uri)
+    const response = await fetch(uri);
+    //Recuperamos el blob de la imagen
+    const blob = await response.blob();
+    //Guardamos en el storage
+    const ref = firebase.storage().ref().child(`avatar/${uid}`);
+    //Subimos el cambio
+    return ref.put(blob);
+  };
+
+  const updatePhotoUrl = () => {
+    setLoadingText("Actualizando avatar")
+    firebase
+      .storage()
+      .ref(`avatar/${uid}`)
+      .getDownloadURL()
+      .then(async (response) => {
+        //console.log(response)
+        const update = {
+          photoURL: response,
+        };
+
+        await firebase.auth().currentUser.updateProfile(update);
+        setLoading(false)
+      })
+      .catch(() => {
+        setLoading(false)
+        toastRef.current.show("Error al actualizar imagen");
+      });
+  };
 
   return (
     <View style={styles.viewUserInfo}>
-        <Avatar
+      <Avatar
         rounded
         size={"large"}
         showEditButton
         containerStyle={styles.userAvatar}
         source={
-            photoURL 
-            ? {uri: photoURL} 
+          photoURL
+            ? { uri: photoURL }
             : require("../../assets/avatar-default.jpg")
         }
-        onEditPress = {() => changeAvatar()}
-        />
-        <View>
-            <Text style={styles.userName}>{displayName ? displayName : "Anonimo"}</Text>
-            <Text style={styles.userEmail}>{email ? email : "Invitado"}</Text>
-        </View>
+        onEditPress={() => changeAvatar()}
+      />
+      <View>
+        <Text style={styles.userName}>
+          {displayName ? displayName : "Anonimo"}
+        </Text>
+        <Text style={styles.userEmail}>{email ? email : "Invitado"}</Text>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    viewUserInfo:{
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "row",
-        backgroundColor: "#f2f2f2",
-        paddingTop: 30,
-        paddingBottom: 30
-    },
-    userAvatar:{
-        marginRight: 20
-    },
-    userName:{
-        fontWeight: "bold",
-        paddingBottom: 5
-    },
-    userEmail:{
-
-    }
-})
+  viewUserInfo: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    backgroundColor: "#f2f2f2",
+    paddingTop: 30,
+    paddingBottom: 30,
+  },
+  userAvatar: {
+    marginRight: 20,
+  },
+  userName: {
+    fontWeight: "bold",
+    paddingBottom: 5,
+  },
+  userEmail: {},
+});
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -6,25 +6,35 @@ import { useNavigation } from "@react-navigation/native";
 import UserInfo from "../components/account/UserInfo";
 import UserOptions from "../components/account/UserOptions";
 import Toast from "react-native-easy-toast";
+import Loading from "../components/Loading";
 
 export default function Profile() {
   const toastRef = useRef();
   const navigation = useNavigation();
   const [userInfo, setUserInfo] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [loadingText, setLoadingText] = useState(null);
 
   useEffect(() => {
-    (async () =>{
+    (async () => {
       const user = firebase.auth().currentUser;
-      setUserInfo(user)
-    })()
-  }, [])
+      setUserInfo(user);
+    })();
+  }, []);
 
   //Es como un if ternario
   //{variableEvaluar && true}
   return (
     <View style={styles.userInfo}>
-      {userInfo && <UserInfo userInfo={userInfo} toastRef={toastRef}/>}
-      <UserOptions/>
+      {userInfo && (
+        <UserInfo
+          userInfo={userInfo}
+          toastRef={toastRef}
+          setLoading={setLoading}
+          setLoadingText={setLoadingText}
+        />
+      )}
+      <UserOptions />
       <Button
         title={"Cerrar sesión"}
         buttonStyle={styles.btnCloseSesion}
@@ -34,11 +44,8 @@ export default function Profile() {
           navigation.navigate("index");
         }}
       />
-      <Toast 
-      ref={toastRef} 
-      position= "center"
-      opacity={0.8}
-      />
+      <Toast ref={toastRef} position="center" opacity={0.8} />
+      <Loading isVisible={loading} text={loadingText} />
     </View>
   );
 }

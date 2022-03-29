76c0ec0712ca1bdcb22e43bb93c03ca74c230ea8
Vontrailop <62673861+Vontrailop@users.noreply.github.com>
2022-03-29 13:00:04 -0600

    032922 consumiendo servicio

--- src/screens/Smart.js
+++ src/screens/Smart.js
@@ -1,16 +1,35 @@
-import React from "react";
-import { Text, View, Button } from "react-native";
+import React, { useEffect, useState} from "react";
+import { Text, View } from "react-native";
+import { map } from 'lodash'
 
 export default function Smart(props){
     const { navigation } = props;
+    const [data, setData] = useState([])
+    
+    const getExercises =  async() =>{
+        try{
+            const response = await fetch('http://192.168.62.185/smart/ejercicio',{
+                method: "GET",
+                headers: {"Content-Type":"Application/json"}
+            })
+            const json = await response.json();
+            setData(json)
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        getExercises();
+        console.log(data)
+    }, [])
+    
+
     return(
         <View>
-            <Text>Mi smart</Text>
-            <Button
-            title="Ir a Training"
-            onPress={()=>{navigation.navigate("training")}}
-            />
-            
+            {map(data,(element, index) =>(
+                <Text key={index}>{element.ejercicio}</Text>
+            ))}
         </View>
     )
 }
